7aa9443d2bdca096f8d598a4e7ff552664616390
Teodor Golisharski <t.golisharski@abv.bg>
2024-12-17 20:07:08 +0200

    basic skeleton for account operations.

--- MyFitnessPal/MyFitnessPal/MyFitnessPal.cpp
+++ MyFitnessPal/MyFitnessPal/MyFitnessPal.cpp
@@ -1,20 +1,28 @@
-// MyFitnessPal.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+void create_account(std::string first_name, std::string last_name, std::string username, std::string password) {
+    // Creating account
+}
+
+void delete_account(std::string username, std::string password) {
+    // Deleting account
+}
+
+void log_in(std::string username, std::string password) {
+    // logs in existring user
+}
+
+void log_out() {
+    // logs out currently logged in user
+}
+
+void start_guide() {
+    // Printing the menu with main commands and instructions for exploitation.
+}
 
 int main()
 {
+
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
