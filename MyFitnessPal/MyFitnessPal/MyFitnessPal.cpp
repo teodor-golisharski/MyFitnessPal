b753fd0ed4ca5d1658a0f3c5aa9c68bc114fd62f
Teodor Golisharski <t.golisharski@abv.bg>
2024-12-24 12:45:04 -0500

    create_account() function now reads all data needed to create an account. Log string is not presented at the moment.

--- MyFitnessPal/MyFitnessPal/MyFitnessPal.cpp
+++ MyFitnessPal/MyFitnessPal/MyFitnessPal.cpp
@@ -21,6 +21,9 @@
 
 static std::vector<std::string> usernames;
 static std::vector<std::string> logs;
+static double bmr = 0;
+static int maintainCalories = 0;
+static int additional = 0;
 
 static std::string current_user = "";
 
@@ -29,10 +32,11 @@ const std::string LOGS_FILE_NAME = "logs.txt";
 
 void create_account() {
 	std::cout << "----------- Registration -----------" << std::endl;
-	std::string username, password, gender, birthdate, goal, account;
+	std::string username, password, gender, birthdate, goal = "", account;
 
 	std::cout << "Username: ";
 	std::cin >> username;
+
 	std::cout << "\nPassword: ";
 	std::cin >> password;
 
@@ -42,13 +46,17 @@ void create_account() {
 	std::cout << "----------- Personal Details -----------" << std::endl;
 	std::cout << "Age: ";
 	std::cin >> age;
+
 	std::cout << "\nGender: ";
 	std::cin >> gender;
+
 	std::cout << "\nHeight: ";
 	std::cin >> height;
+
 	std::cout << "\nWeight: ";
 	std::cin >> weight;
-	std::cout << "-------------------------------------------------------" << std::endl;
+
+	std::cout << "\n-------------------------------------------------------" << std::endl;
 	std::cout << "1 - Sedentary (little to no exercise)";
 	std::cout << "2 - Light activity (exercise 1-3 days per week)";
 	std::cout << "3 - Moderate activity (exercise 3-5 days per week)";
@@ -56,9 +64,51 @@ void create_account() {
 	std::cout << "5 - Very active (intense workouts or physical labor)";
 	std::cout << "#Guide: Type the number based on your activity.";
 	std::cout << "-------------------------------------------------------" << std::endl;
-	std::cout << "\nActivity level: ";
+	std::cout << "Activity level: ";
 	std::cin >> activity_level;
 
+	switch (activity_level) {
+	case 1: bmr = 1.2; break;
+	case 2: bmr = 1.375; break;
+	case 3: bmr = 1.55; break;
+	case 4: bmr = 1.725; break;
+	case 5: bmr = 1.9; break;
+	}
+
+	std::cout << "\n-------------------------------------------------------" << std::endl;
+	std::cout << "1 - Lose weight" << std::endl;
+	std::cout << "2 - Maintain weight" << std::endl;
+	std::cout << "3 - Gain weight" << std::endl;
+	std::cout << "#Guide: Type the number based on your goal." << std::endl;
+	std::cout << "-------------------------------------------------------" << std::endl;
+	int tempOption1 = 0, tempOption2 = 0;
+	std::cout << "Goal: ";
+	std::cin >> tempOption1;
+	if (tempOption1 != 2) {
+		std::cout << "\n-------------------------------------------------------" << std::endl;
+		std::cout << "1 - 0.25 kg a week" << std::endl;
+		std::cout << "2 - 0.50 kg a week" << std::endl;
+		std::cout << "3 - 0.75 kg a week" << std::endl;
+		std::cout << "4 - 1 kg a week" << std::endl;
+		std::cout << "#Guide: Type the number based on your desired transformation rate.";
+		std::cout << "-------------------------------------------------------" << std::endl;
+		std::cout << "Rate: ";
+		std::cin >> tempOption2;
+
+		tempOption2 = tempOption2 == 1 ? 275 : tempOption2 == 2 ? 550 : tempOption2 == 3 ? 825 : 1110;
+		if (tempOption1 == 1) {
+			tempOption2 = 0 - tempOption2;
+		}
+	}
+	additional += tempOption2;
+
+	std::cout << "\n-------------------------------------------------------" << std::endl;
+	std::cout << "------------------CHOOSE ACCOUNT TYPE------------------" << std::endl;
+	std::cout << "1 - Standard" << std::endl;
+	std::cout << "2 - Premium" << std::endl;
+	std::cout << "-------------------------------------------------------" << std::endl;
+	std::cout << "AccountType: ";
+	std::cin >> account;
 }
 
 void delete_account(std::string username, std::string password) {
@@ -125,5 +175,6 @@ void run() {
 
 int main()
 {
-	run();
+	std::cout << "\n-------------------------------------------------------" << std::endl;
+	//run();
 }
