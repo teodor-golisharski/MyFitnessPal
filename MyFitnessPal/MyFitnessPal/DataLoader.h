d3b34184f1907809fc59966bf3662be586bf898f
Teodor Golisharski <t.golisharski@gmail.com>
2025-01-19 02:45:47 +0200

    Included a consistent header in each file with project details.

--- MyFitnessPal/MyFitnessPal/DataLoader.h
+++ MyFitnessPal/MyFitnessPal/DataLoader.h
@@ -1,3 +1,17 @@
+/**
+*
+* Solution to course project # 9
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2024/2025
+*
+* @author Teodor Golisharski
+* @idnumber 6MI0600367 @compiler VC
+*
+* <loading data file>
+*
+*/
+
 #pragma once
 #include <vector>
 #include <string>
--- MyFitnessPal/MyFitnessPal/OutputMessages.h
+++ MyFitnessPal/MyFitnessPal/OutputMessages.h
@@ -1,3 +1,17 @@
+/**
+*
+* Solution to course project # 9
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2024/2025
+*
+* @author Teodor Golisharski
+* @idnumber 6MI0600367 @compiler VC
+*
+* <file for output messages>
+*
+*/
+
 #pragma once
 #include <string>
 
